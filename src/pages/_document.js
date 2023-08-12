8c291ae070c593dd1f4989b27b136e83e2e5a110
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-08-12 21:57:46 +0900

    edit: <Head>タグをPWA仕様に変更

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,7 +3,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="application-name" content="BocchiTalk" />
+        <meta name="theme-color" content="#fff" />
+        <link rel="icon" sizes="192x192" href="../../public/512x512_Bocchi-Talk.png" />
+        <link rel="icon" href="../../public/favicon.ico" />
+        <link rel="manifest" href="../../public/manifest.json" />
+      </Head>
       <body>
         <Main />
         <NextScript />
