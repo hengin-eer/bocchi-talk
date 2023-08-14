56443f552b9e84b0fa8d5b3e4a902e0c6d0f5cc5
hengin-eer <hirokix0608@gmail.com>
2023-08-15 00:02:39 +0900

    fix: #23 グラデーションテキストをカスタムコンポーネントに差し替え

--- src/components/page-header.jsx
+++ src/components/page-header.jsx
@@ -1,12 +1,13 @@
 import { Button, ButtonGroup, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import { GradientText } from './custom-chakra-ui'
 
 export const PageHeader = () => {
     return (
         <Flex align='center' justify='space-between' h='80px' w='100%' px={30} bg={'blackAlpha.50'}>
             <Flex align='center'>
                 <Image src={'/512x512_Bocchi-Talk.png'} alt='Bocchi Talk' boxSize='80px' />
-                <Text fontSize='4xl' fontWeight='bold' bgClip='text' bgGradient='linear(to-br, #1DBEE1, #487EE7, #884FE4)'>Bocchi Talk</Text>
+                <GradientText fontSize='4xl' fontWeight='bold'>Bocchi Talk</GradientText>
             </Flex>
             <ButtonGroup gap={2}>
                 <Button variant='outline' colorScheme='cyan'>Sign Up</Button>
