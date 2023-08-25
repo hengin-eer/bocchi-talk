a9743878e2c06ab9b464eadede275df56615edf7
hengin-eer <hirokix0608@gmail.com>
2023-08-25 20:01:28 +0900

    💄 #35 roleごとにメッセージを区別しやすいように、アイコンの追加、表示位置を変更

--- src/components/chat-area.jsx
+++ src/components/chat-area.jsx
@@ -1,4 +1,4 @@
-import { Box, Button, Fade, Flex, Input, Text, useDisclosure } from '@chakra-ui/react'
+import { Box, Button, Fade, Flex, Image, Input, Text, useDisclosure } from '@chakra-ui/react'
 import React, { use, useEffect, useState } from 'react'
 
 export const ChatArea = () => {
@@ -7,6 +7,7 @@ export const ChatArea = () => {
 		role: "system",
 		content: "system_prompt" // 初期値としてシステムメッセージを入れておく。
 	}]); // 初期値の設定
+
 	const { isOpen, onOpen, onClose } = useDisclosure();
 	const [menuIndex, setMenuIndex] = useState(null); // 右クリックされたときのメニューのindexを保持する
 	const [viewportHeight, setViewportHeight] = useState(100);
@@ -72,12 +73,27 @@ export const ChatArea = () => {
 		};
 	}, []);
 
+	console.log(chats)
+
 	return (
 		<Box height={`calc(${viewportHeight}px - ${62}px)`} overflowY='hidden' onClick={() => onClose()}> {/* クリックしたときにメニューを閉じる */}
-			<Flex direction='column' align='flex-end' rowGap={3} h={`calc(${viewportHeight}px - ${132}px)`} px={4} py='4' bg={'blue.200'} my={0} overflowY='auto'>
+			<Flex direction='column' align='center' rowGap={3} h={`calc(${viewportHeight}px - ${132}px)`} px={4} py='4' bg={'blue.200'} my={0} overflowY='auto'>
 				{chats.slice(1, chats.length).map((message, index) => (
-					<Box pos='relative' key={index} onContextMenu={(e) => handleRightClick(e, index)}>
-						<Text w='max-content' maxW='70vw' px={5} py={3} bg={'white'} borderRadius='20px 0px 20px 20px'>{message.content}</Text>
+					<Flex key={index} onContextMenu={(e) => handleRightClick(e, index)}
+						pos='relative' flexDirection={message.role === "user" ? 'row-reverse' : 'row'} align='flex-start' columnGap='10px'
+						marginLeft={message.role === "user" ? 'auto' : '0px'}
+						marginRight={message.role === "user" ? '0px' : 'auto'}
+					>
+						{message.role === "user" ?
+							<Image src='/face.jpg' w='40px' h='40px' borderRadius='50%' />
+							:
+							<Image src='/BocchiTalk-android-chrome-72x72.png' w='40px' h='40px' borderRadius='50%' bg='white' />
+						}
+						<Text w='max-content' maxW='70vw' px={5} py={3} bg={'white'}
+							borderRadius={message.role === "user" ? '20px 0px 20px 20px' : '0px 20px 20px 20px'}
+						>
+							{message.content}
+						</Text>
 						{index === menuIndex && (
 							<Box pos='absolute' zIndex={999} top='40px' right={0}>
 								<Fade in={isOpen} key={index}>
@@ -91,7 +107,7 @@ export const ChatArea = () => {
 								</Fade>
 							</Box>
 						)}
-					</Box>
+					</Flex>
 				))}
 			</Flex>
 			<form onSubmit={(e) => sendMessage(e)} >
