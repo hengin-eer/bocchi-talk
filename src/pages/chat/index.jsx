ceeb20464fdeceed0c7dc71f5e5aac641496a24a
hengin-eer <hirokix0608@gmail.com>
2023-08-26 23:18:51 +0900

    :sparkles: feat: #38 音声入力言語のstateを追加

--- src/pages/chat/index.jsx
+++ src/pages/chat/index.jsx
@@ -1,13 +1,14 @@
 import { AppHeader } from '@/components/app-header'
 import { ChatArea } from '@/components/chat-area'
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function chat() {
+	const [speechLanguage, setSpeechLanguage] = useState('en-US')
 	return (
 		<div>
-			<AppHeader />
-			<ChatArea />
+			<AppHeader speechLanguage={speechLanguage} setSpeechLanguage={setSpeechLanguage} />
+			<ChatArea speechLanguage={speechLanguage} />
 		</div>
 	)
 }
