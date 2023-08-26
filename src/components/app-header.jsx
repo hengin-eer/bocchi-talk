a2608bcb62375c358c5bb92e0e0a3acf3e936fbf
hengin-eer <hirokix0608@gmail.com>
2023-08-26 23:23:56 +0900

    ✨ #38 Selectで`speechLanguage`を指定できるように AppMenuコンポーネントを廃止して統合

--- src/components/app-header.jsx
+++ src/components/app-header.jsx
@@ -1,24 +1,22 @@
 import { ChevronLeftIcon, HamburgerIcon } from '@chakra-ui/icons'
-import { Flex, Text } from '@chakra-ui/react'
+import { Flex, Select, Text } from '@chakra-ui/react'
 import Link from 'next/link'
 import React from 'react'
 
-export const AppHeader = () => {
+export const AppHeader = ({ speechLanguage, setSpeechLanguage }) => {
 	return (
-			<Flex h='32px' mx={5} my={3} align='center' justify='space-between'>
-				<Link href='/'>
-					<ChevronLeftIcon boxSize={8} />
-				</Link>	
-				<Text fontSize={18}>Chat Title</Text>
-				<AppMenu />
+		<Flex h='32px' mx={5} my={3} align='center' justify='space-between'>
+			<Link href='/'>
+				<ChevronLeftIcon boxSize={8} />
+			</Link>
+			<Text fontSize={18}>Chat Title</Text>
+			<Flex align='center' columnGap={2}>
+				<Select value={speechLanguage} placeholder='Language' variant='filled' size='sm' onChange={(e) => setSpeechLanguage(e.target.value)}>
+					<option value='en-US'>en-US</option>
+					<option value='ja'>ja</option>
+				</Select>
+				<HamburgerIcon boxSize={6} />
 			</Flex>
-	)
-}
-
-export const AppMenu = () => {
-	return (
-		<div>
-			<HamburgerIcon boxSize={6} />
-		</div>
+		</Flex>
 	)
 }
