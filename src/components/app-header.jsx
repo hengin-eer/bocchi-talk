ab1d34c6674d46ba5b7bf21c8ade3be88f3ce273
hengin-eer <hirokix0608@gmail.com>
2023-08-28 23:53:41 +0900

    ✨ feat: #41 新たに韓国語・中国語・台湾語・フランス語・ドイツ語・タイ語を音声入力言語対応

--- src/components/app-header.jsx
+++ src/components/app-header.jsx
@@ -12,8 +12,14 @@ export const AppHeader = ({ speechLanguage, setSpeechLanguage }) => {
 			<Text fontSize={18}>Chat Title</Text>
 			<Flex align='center' columnGap={2}>
 				<Select value={speechLanguage} placeholder='Language' variant='filled' size='sm' onChange={(e) => setSpeechLanguage(e.target.value)}>
-					<option value='en-US'>en-US</option>
-					<option value='ja'>ja</option>
+					<option value='en-US'>English(US)</option>
+					<option value='ja'>日本語</option>
+					<option value='ko'>한국어(韓国語)</option>
+					<option value='zh-CN'>中文(中国語)</option>
+					<option value='zh-TW'>中文(台湾語)</option>
+					<option value='fr-FR'>French(フランス語)</option>
+					<option value='de-DE'>German(ドイツ語)</option>
+					<option value='th-TH'>ภาษาไทย(タイ語)</option>
 				</Select>
 				<HamburgerIcon boxSize={6} />
 			</Flex>
