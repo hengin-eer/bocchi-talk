2cccc22f9db7444a56f43505c53432dbde1e48a4
hengin-eer <hirokix0608@gmail.com>
2023-09-01 23:11:38 +0900

    ✨ feat: #44 Firestore関連の`hooks`を追加

---
+++ src/hooks/useFirestore.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react"
+import db from "../lib/firebase"
+import { collection, addDoc, getDocs } from "firebase/firestore"
+
+export const useFirestore = () => {
+    // const addFirestoreChat = async (newChat)
+
+    const addFirestoreDoc = async (newMessage, chatsId, index) => {
+        const mRef = collection(db, "chats", chatsId, "messages")
+        await addDoc(mRef, {
+            content: newMessage.content,
+            role: newMessage.role,
+            id: index,
+        })
+        console.log(`
+            content: ${newMessage.content},
+            role: ${newMessage.role},
+            id: ${index},
+        `)
+    }
+
+    const getMessages = async (chatsId) => {
+        const snapshot = await getDocs(collection(db, 'chats', chatsId, 'messages'))
+        const getData = snapshot.docs.map((doc) => {
+            const data = doc.data()
+            data.strId = doc.id
+            return data
+        })
+        return getData
+    }
+
+    const useMessages = async (chatsId) => {
+        const [messages, setMessages] = useState([])
+        useEffect(() => {
+            ; (async () => {
+                const data = await getMessages(chatsId)
+                setMessages(data)
+            })()
+        }, [])
+        return messages
+    }
+
+    const getUser = async () => {
+        const snapshot = await getDocs(collection(db, 'users'))
+        const getData = snapshot.docs.map((doc) => {
+            const data = doc.data()
+            data.id = doc.id
+            return data
+        })
+        return getData
+    }
+
+    const useUser = () => {
+        const [users, setUsers] = useState([])
+        useEffect(() => {
+            ; (async () => {
+                const data = await getUser()
+                setUsers(data)
+            })()
+        }, [])
+
+        return users
+    }
+
+    return { addFirestoreDoc, useMessages, useUser }
+}
