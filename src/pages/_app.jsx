2298556a2c225f4842fd6b3715856bfaf6c996de
hengin-eer <hirokix0608@gmail.com>
2023-09-05 14:10:52 +0900

    ✨ feat: #54 認証コンテキストを扱うプロバイダーの設置

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,11 +1,14 @@
 import '@/styles/globals.css'
 import 'regenerator-runtime'
 import { ChakraProvider } from '@chakra-ui/react'
+import { AuthProvider } from '@/hooks/useFirebaseAuth'
 
 export default function App({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <AuthProvider>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </AuthProvider>
   )
 }
