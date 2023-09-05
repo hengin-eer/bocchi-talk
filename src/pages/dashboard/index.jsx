42d17c7846019af0054fc55de52b0549e59bc41d
hengin-eer <hirokix0608@gmail.com>
2023-09-05 15:59:08 +0900

    💄 style: #27 チャット履歴のUIを調整

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,25 +1,37 @@
+import { DashboardNav } from '@/components/dashboard-nav'
+import { useAuth } from '@/hooks/useFirebaseAuth'
 import { useFirestore } from '@/hooks/useFirestore'
-import { Heading, Text } from '@chakra-ui/react'
+import { Box, Flex, Heading, Skeleton, Text } from '@chakra-ui/react'
 import Link from 'next/link'
 import React from 'react'
 
 export default function Dashboard() {
 	const { useChatsIds } = useFirestore()
 	const chatsIds = useChatsIds()
-	let chatsUpdatedAt
-	console.log(chatsIds)
+	const user = useAuth()
 
 	return (
-		<div>
-			<Heading as='h1' size='lg' mb={5}>Dashboard</Heading>
-			<ul>
-				{chatsIds.map((chatsId) => (
-					<Link key={chatsId} href={`/chat/${chatsId.id}`}>
-						<Text fontSize='md'>{chatsId.id}</Text>
-						<Text fontSize='sm' color='gray.400'>{new Date(chatsId.updatedAt.seconds * 1000).toLocaleString()}</Text>
-					</Link>
-				))}
-			</ul>
-		</div>
+		<Flex>
+			<DashboardNav />
+			<Box w='full' p='20px'>
+				<Heading as='h1' size='lg'>Chats</Heading>
+				<Flex direction='column' align='flex-start' rowGap='20px' py='20px'>
+					{user === null &&
+						<>
+							<Skeleton h='65px' w='full' borderRadius='10px'></Skeleton>
+							<Skeleton h='65px' w='full' borderRadius='10px'></Skeleton>
+						</>
+					}
+					{user && chatsIds.map((chatsId) => (
+						<Box w='full' px='30px' py='10px' bg='gray.200' borderRadius='10px'>
+							<Link key={chatsId} href={`/chat/${chatsId.id}`}>
+								<Text fontSize='md'>{chatsId.id}</Text>
+								<Text fontSize='sm' color='gray.400'>{new Date(chatsId.updatedAt.seconds * 1000).toLocaleString()}</Text>
+							</Link>
+						</Box>
+					))}
+				</Flex>
+			</Box>
+		</Flex>
 	)
 }
