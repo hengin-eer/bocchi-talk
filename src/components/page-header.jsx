fc4a5dcdde52c41965fe4b9ee717e81049199a98
hengin-eer <hirokix0608@gmail.com>
2023-09-05 15:45:43 +0900

    ✨ feat: #54 トップページのヘッダーにログイン機能を追加

--- src/components/page-header.jsx
+++ src/components/page-header.jsx
@@ -1,8 +1,11 @@
 import { Button, ButtonGroup, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import { GradientText } from './custom-chakra-ui'
+import { useAuth } from '@/hooks/useFirebaseAuth'
+import { login, logout } from '@/lib/auth'
 
 export const PageHeader = () => {
+    const user = useAuth();
     return (
         <Flex align='center' justify='space-between' h='80px' w='100%' px={30} bg={'blackAlpha.50'}>
             <Flex align='center'>
@@ -10,8 +13,8 @@ export const PageHeader = () => {
                 <GradientText fontSize='4xl' fontWeight='bold'>Bocchi Talk</GradientText>
             </Flex>
             <ButtonGroup gap={2}>
-                <Button variant='outline' colorScheme='cyan'>Sign Up</Button>
-                <Button colorScheme='green'>Login</Button>
+                {user === null && <Button colorScheme='green' onClick={login}>Login</Button>}
+                {user && <Button colorScheme='red' onClick={logout}>Logout</Button>}
             </ButtonGroup>
         </Flex>
     )
