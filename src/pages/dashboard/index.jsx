c6c8cf3183b600a00f2116d702b32a42c04a8fea
hengin-eer <hirokix0608@gmail.com>
2023-09-12 14:29:07 +0900

    ✨ feat: #54 `next-auth`を用いてサーバー側でセッション確認するように変更 その変更に伴う調整

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,30 +1,29 @@
 import { DashboardNav } from '@/components/dashboard-nav'
-import { useAuth, useRedirectIsLogout } from '@/hooks/useFirebaseAuth'
 import { useFirestore } from '@/hooks/useFirestore'
 import { Box, Flex, Heading, Skeleton, Text } from '@chakra-ui/react'
+import { getServerSession } from 'next-auth'
 import Link from 'next/link'
-import React, { useEffect, useState } from 'react'
+import { authOptions } from '../api/auth/[...nextauth]'
 
-export default function Dashboard() {
-	const user = useAuth()
+export default function Dashboard({ currentUser }) {
 	const { useChatsIds } = useFirestore()
-	useRedirectIsLogout(user)
-	
-	const chatsIds = useChatsIds(user)
+	console.log(currentUser)
+
+	const chatsIds = useChatsIds(currentUser.email)
 
 	return (
 		<Flex>
-			<DashboardNav />
+			<DashboardNav user={currentUser} />
 			<Box w='full' p='20px'>
 				<Heading as='h1' size='lg'>Chats</Heading>
 				<Flex direction='column' align='flex-start' rowGap='20px' py='20px'>
-					{user === undefined &&
+					{!currentUser &&
 						<>
 							<Skeleton h='65px' w='full' borderRadius='10px'></Skeleton>
 							<Skeleton h='65px' w='full' borderRadius='10px'></Skeleton>
 						</>
 					}
-					{user && chatsIds.map((chatsId) => (
+					{currentUser && chatsIds.map((chatsId) => (
 						<Box w='full' px='30px' py='10px' bg='gray.200' borderRadius='10px'>
 							<Link key={chatsId} href={`/chat/${chatsId.id}`}>
 								<Text fontSize='md'>{chatsId.id}</Text>
@@ -37,3 +36,28 @@ export default function Dashboard() {
 		</Flex>
 	)
 }
+
+export const getServerSideProps = async (context) => {
+	const session = await getServerSession(context.req, context.res, authOptions);
+
+	if (!session) {
+		return {
+			redirect: {
+				destination: '/',
+				permanent: false,
+			},
+		};
+	}
+
+	const currentUser = {
+		name: session.user.name,
+		email: session.user.email,
+		image: session.user.image,
+	}
+
+	if (session) {
+		return {
+			props: { currentUser },
+		};
+	}
+};
