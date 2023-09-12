fddc20e709c5a5b7b2c92b11f4e79e59105b3fdd
hengin-eer <hirokix0608@gmail.com>
2023-09-12 14:33:10 +0900

    ✨ feat: #54 ログイン実装の変更に伴う調整 未ログイン時のUI削除

--- src/components/dashboard-nav.jsx
+++ src/components/dashboard-nav.jsx
@@ -1,28 +1,22 @@
-import { useAuth } from '@/hooks/useFirebaseAuth';
-import { login, logout } from '@/lib/auth';
-import { Avatar, Box, Button, Flex, Icon, SkeletonCircle, Text } from '@chakra-ui/react'
+import { Avatar, Button, Flex, Icon, SkeletonCircle, Text } from '@chakra-ui/react'
+import { signOut } from 'next-auth/react';
 import Link from 'next/link'
-import React, { useState } from 'react'
-import { PiSignInFill, PiSignOutFill } from 'react-icons/pi';
+import { PiSignOutFill } from 'react-icons/pi';
 
-export const DashboardNav = () => {
-	const user = useAuth();
-	console.log(user);
-
+export const DashboardNav = ({ user }) => {
 	return (
 		<Flex direction='column' align='center' justify='space-between' h='100vh' w='80px' py='30px' bg='greenyellow'>
-			{user ? <Avatar name={user.name} src={user.pic} boxSize='50px' /> : <SkeletonCircle size='50px' />}
+			{user ?
+			<Avatar name={user.name} src={user.image} boxSize='50px' />
+			:
+			<SkeletonCircle size='50px' />
+			}
 			<Flex direction='column' align='center' rowGap='30px' pb='50px'>
 				<Link href='/'>
 					<Text fontSize='md'>Home</Text>
 				</Link>
-				{user === null &&
-					<Button variant='link' colorScheme='green' onClick={login}>
-						<Icon boxSize='30px' as={PiSignInFill} />
-					</Button>
-				}
 				{user &&
-					<Button variant='link' colorScheme='red' onClick={logout}>
+					<Button variant='link' colorScheme='red' onClick={() => signOut()}>
 						<Icon boxSize='30px' as={PiSignOutFill} />
 					</Button>
 				}
