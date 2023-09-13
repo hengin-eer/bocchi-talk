31c587d0733e2ff2785553b586941d1826853211
hengin-eer <hirokix0608@gmail.com>
2023-09-13 16:16:24 +0900

    ✨ feat: #54 ローディングプロバイダーの読み込み

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -2,12 +2,15 @@ import '@/styles/globals.css'
 import 'regenerator-runtime'
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
+import { LoadingProvider } from '@/hooks/useLoading'
 
 export default function App({ Component, pageProps }) {
   return (
     <SessionProvider>
       <ChakraProvider>
-        <Component {...pageProps} />
+        <LoadingProvider>
+          <Component {...pageProps} />
+        </LoadingProvider>
       </ChakraProvider>
     </SessionProvider>
   )
