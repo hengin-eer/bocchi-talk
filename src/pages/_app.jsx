fd8d1271c525e121787ec4e8cbea8e401cf216a4
hengin-eer <hirokix0608@gmail.com>
2023-09-16 19:02:04 +0900

    ✨ feat: #64 `Recoil`を使用するためのプロバイダーの追加

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -3,15 +3,18 @@ import 'regenerator-runtime'
 import { ChakraProvider } from '@chakra-ui/react'
 import { SessionProvider } from 'next-auth/react'
 import { LoadingProvider } from '@/hooks/useLoading'
+import { RecoilRoot } from 'recoil'
 
 export default function App({ Component, pageProps }) {
   return (
-    <SessionProvider>
-      <ChakraProvider>
-        <LoadingProvider>
-          <Component {...pageProps} />
-        </LoadingProvider>
-      </ChakraProvider>
-    </SessionProvider>
+    <RecoilRoot>
+      <SessionProvider>
+        <ChakraProvider>
+          <LoadingProvider>
+            <Component {...pageProps} />
+          </LoadingProvider>
+        </ChakraProvider>
+      </SessionProvider>
+    </RecoilRoot>
   )
 }
