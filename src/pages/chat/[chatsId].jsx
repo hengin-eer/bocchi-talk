3df30196e34ab2d7097d70991972f218ed37e5f0
hengin-eer <hirokix0608@gmail.com>
2023-09-23 13:53:41 +0900

    ✨ feat: #71 チャットタイトルを取得する

--- src/pages/chat/[chatsId].jsx
+++ src/pages/chat/[chatsId].jsx
@@ -1,40 +1,49 @@
 import { AppHeader } from '@/components/app-header'
 import { ChatArea } from '@/components/chat-area'
 import { db } from '@/lib/firebase'
+import { chatTitleState, chatsDataState } from '@/states/chatsDataState'
 import { currentUserState } from '@/states/currentUserState'
-import { collection, getDocs } from 'firebase/firestore'
+import { collection, doc, getDoc, getDocs } from 'firebase/firestore'
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
-import { useRecoilValue } from 'recoil'
+import { useRecoilState, useRecoilValue } from 'recoil'
 
 export default function chat() {
 	const [messages, setMessages] = useState([])
 	const [isFetched, setIsFetched] = useState(false)
 	const currentUser = useRecoilValue(currentUserState)
+	const chatsData = useRecoilValue(chatsDataState)
+	const [chatTitle, setChatTitle] = useRecoilState(chatTitleState)
 
 	const router = useRouter()
-	useEffect(() => {
-		if (router.isReady && !isFetched && currentUser) {
+	if (router.isReady && !isFetched && currentUser) {
+		const id = router.query.chatsId
+		if (chatsData.length !== 0) setChatTitle(chatsData.filter((chat) => chat.id === id).map((chat) => chat.title)[0])
+		if (chatsData.length === 0) {
 			; (async () => {
-				const id = router.query.chatsId
-				const colRef = collection(db, 'users', currentUser.email, 'chats', id, 'messages');
-				const snapShots = await getDocs(colRef)
-
-				const docs = snapShots.docs.map((doc) => {
-					const data = doc.data()
-					return data
-				})
-				setMessages(docs.sort((a, b) => a.createdAt.seconds - b.createdAt.seconds))
+				const snapshot = await getDoc(doc(db, 'users', currentUser.email, 'chats', id))
+				const data = snapshot.data()
+				setChatTitle(data.title)
 			})()
-			setIsFetched(true)
-			console.log(currentUser)
 		}
-	}, [router, currentUser])
+
+		; (async () => {
+			const colRef = collection(db, 'users', currentUser.email, 'chats', id, 'messages');
+			const snapShots = await getDocs(colRef)
+
+			const docs = snapShots.docs.map((doc) => {
+				const data = doc.data()
+				return data
+			})
+			setMessages(docs.sort((a, b) => a.createdAt.seconds - b.createdAt.seconds))
+		})()
+		setIsFetched(true)
+	}
 	const chatsId = router.query.chatsId
 
 	return (
 		<div>
-			<AppHeader/>
+			<AppHeader chatTitle={chatTitle} />
 			<ChatArea firestoreMessages={messages} chatsId={chatsId} currentUser={currentUser && currentUser} />
 		</div>
 	)
