387fd8e1397b1e88f4ba2b478feda40073c8b435
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-09-23 17:59:17 +0900

    ✨ #40 辞書をじっそうするためのベースを追加しました

--- src/components/app-header.jsx
+++ src/components/app-header.jsx
@@ -6,6 +6,7 @@ import { PiArchiveBoxBold, PiFacebookLogoBold, PiGearSixBold, PiGearSixFill, PiL
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import { useRecoilState } from 'recoil'
 import { speechLanguageState } from '@/states/speechLanguageState'
+import { AutoResizeTextarea } from '@/components/custom-chakra-ui'
 
 export const AppHeader = ({ chatTitle }) => {
 	const [speechLanguage, setSpeechLanguage] = useRecoilState(speechLanguageState)
@@ -64,7 +65,36 @@ export const AppHeader = ({ chatTitle }) => {
 
 								<TabPanels>
 									<TabPanel>
-										<Text>翻訳</Text>
+										<Box>
+											<Flex>
+												<Text mr='1rem' fontSize='lg'>Before</Text>
+												<Select value={speechLanguage} placeholder='Language' variant='filled' size='sm' onChange={(e) => setSpeechLanguage(e.target.value)}>
+													<option value='en-US'>English(US)</option>
+													<option value='ja'>日本語</option>
+													<option value='ko'>한국어(韓国語)</option>
+													<option value='zh-CN'>中文(中国語)</option>
+													<option value='zh-TW'>中文(台湾語)</option>
+													<option value='fr-FR'>French(フランス語)</option>
+													<option value='de-DE'>German(ドイツ語)</option>
+													<option value='th-TH'>ภาษาไทย(タイ語)</option>
+												</Select>
+											</Flex>
+											<AutoResizeTextarea mt='1rem'></AutoResizeTextarea>
+
+											<Flex mt='1rem'>
+												<Text mr='1rem' fontSize='lg'>After</Text>
+												<Select value={speechLanguage} placeholder='Language' variant='filled' size='sm' onChange={(e) => setSpeechLanguage(e.target.value)}>
+													<option value='en-US'>English(US)</option>
+													<option value='ja'>日本語</option>
+													<option value='ko'>한국어(韓国語)</option>
+													<option value='zh-CN'>中文(中国語)</option>
+													<option value='zh-TW'>中文(台湾語)</option>
+													<option value='fr-FR'>French(フランス語)</option>
+													<option value='de-DE'>German(ドイツ語)</option>
+													<option value='th-TH'>ภาษาไทย(タイ語)</option>
+												</Select>
+											</Flex>
+										</Box>
 									</TabPanel>
 									<TabPanel>
 										<Text>単語帳</Text>
