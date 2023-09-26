49ee6149493a7b3fbb8d447d26a2a842bd116d23
hengin-eer <hirokix0608@gmail.com>
2023-09-26 23:41:30 +0900

    ✨ feat: #78 校正した文章を返すAPIの作成

---
+++ src/pages/api/proofread/index.js
@@ -0,0 +1,55 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const config = new Configuration({
+	apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+
+export default async function handler(req, res) {
+	if (!config.apiKey) {
+		res.status(500).json({
+			error: {
+				message: "OpenAI API key is not defined",
+			},
+		});
+		return;
+	}
+
+	const message = [{
+		role: "user",
+		content:
+		`// Proofread the following sentences.
+		
+		Before: hallo, wolld!
+		After: Hello, world!
+
+		Before: this is an pens.
+		After: This is a pen.
+
+		Before: ${req.body.message}
+		After: `
+	}];
+
+	try {
+		const completion = await openai.createChatCompletion({
+			model: "gpt-3.5-turbo",
+			messages: message,
+			temperature: 0.7,
+			max_tokens: 200,
+		});
+
+		res.status(200).json({ result: completion.data.choices[0].message });
+	} catch (error) {
+		if (error.response) {
+			console.error(error.response.status, error.response.data);
+			res.status(error.response.status).json(error.response.data);
+		} else {
+			console.error(`Error with OpenAI API request: ${error.message}`);
+			res.status(500).json({
+				error: {
+					message: "An error occurred during your request.",
+				},
+			});
+		}
+	}
+}
