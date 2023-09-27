ca25734eae4e53166a6366320dcdcfcbf1f0c2be
hengin-eer <hirokix0608@gmail.com>
2023-09-27 20:07:37 +0900

    🐛 fix: #86 プロフィールボタンのスタイルを変更、リンクの種類を変更

--- src/components/dashboard-nav.jsx
+++ src/components/dashboard-nav.jsx
@@ -1,9 +1,9 @@
 import {
-	Button, 
-	Flex, 
-	Icon, 
-	SkeletonCircle, 
-	Text, 
+	Button,
+	Flex,
+	Icon,
+	SkeletonCircle,
+	Text,
 	Menu,
 	MenuButton,
 	MenuList,
@@ -19,12 +19,19 @@ export const DashboardNav = ({ user }) => {
 		<Flex direction={{ base: 'row', sm: 'column' }} align='center' justify='space-between' h={{ base: '80px', sm: '100dvh' }} w={{ base: '100%', sm: '80px' }} mt={{ base: 'auto', sm: '0px' }} p={{ base: '0px 30px', sm: '30px 0px' }} bg='greenyellow'>
 			{user ?
 				<Menu>
-					<MenuButton as={Button} p={0} bg="transparent">
+					<MenuButton variant='unstyled' p={0} bg="transparent">
 						<Image src={user.image} alt={user.name} boxSize="50px" borderRadius="50%" />
 					</MenuButton>
 					<MenuList>
-						<MenuItem as='a' href='/settings' icon={<Icon boxSize='30px' as={PiGearSixFill} />}>Settings</MenuItem>
-						<MenuItem onClick={ () => signOut({redirect: true, callbackUrl: '/'}) } icon={<Icon boxSize='30px' as={PiSignOutFill} />}>LogOut</MenuItem>
+						<MenuItem>
+							<Link href='/settings'>
+								<Flex align='center' columnGap='12px'>
+									<Icon boxSize='30px' color='slategray' as={PiGearSixFill} />
+									<Text>Settings</Text>
+								</Flex>
+							</Link>
+						</MenuItem>
+						<MenuItem onClick={() => signOut({ redirect: true, callbackUrl: '/' })} icon={<Icon boxSize='30px' color='red' as={PiSignOutFill} />}>LogOut</MenuItem>
 					</MenuList>
 				</Menu>
 				:
