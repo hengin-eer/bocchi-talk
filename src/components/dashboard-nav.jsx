23961cc5b3f302fcc990d3e326de255e10a874d6
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-09-27 19:29:34 +0900

    ✨ #86 アイコンを収納

--- src/components/dashboard-nav.jsx
+++ src/components/dashboard-nav.jsx
@@ -1,5 +1,15 @@
-import { Image } from '@chakra-ui/react';
-import { Button, Flex, Icon, SkeletonCircle, Text } from '@chakra-ui/react'
+import {
+	Button, 
+	Flex, 
+	Icon, 
+	SkeletonCircle, 
+	Text, 
+	Menu,
+	MenuButton,
+	MenuList,
+	MenuItem,
+	Image,
+} from '@chakra-ui/react'
 import { signOut } from 'next-auth/react';
 import Link from 'next/link'
 import { PiGearSixFill, PiHeartFill, PiHouseFill, PiPaperPlaneTiltFill, PiSignOutFill } from 'react-icons/pi';
@@ -8,7 +18,15 @@ export const DashboardNav = ({ user }) => {
 	return (
 		<Flex direction={{ base: 'row', sm: 'column' }} align='center' justify='space-between' h={{ base: '80px', sm: '100dvh' }} w={{ base: '100%', sm: '80px' }} mt={{ base: 'auto', sm: '0px' }} p={{ base: '0px 30px', sm: '30px 0px' }} bg='greenyellow'>
 			{user ?
-				<Image src={user.image} alt={user.name} boxSize='50px' borderRadius='50%' />
+				<Menu>
+					<MenuButton as={Button} p={0} bg="transparent">
+						<Image src={user.image} alt={user.name} boxSize="50px" borderRadius="50%" />
+					</MenuButton>
+					<MenuList>
+						<MenuItem as='a' href='/settings' icon={<Icon boxSize='30px' as={PiGearSixFill} />}>Settings</MenuItem>
+						<MenuItem onClick={ () => signOut({redirect: true, callbackUrl: '/'}) } icon={<Icon boxSize='30px' as={PiSignOutFill} />}>LogOut</MenuItem>
+					</MenuList>
+				</Menu>
 				:
 				<SkeletonCircle size='50px' />
 			}
@@ -27,20 +45,10 @@ export const DashboardNav = ({ user }) => {
 						</Link>
 					</Button>
 					<Button display='block' variant='link' colorScheme='cyan'>
-						<Link href='/settings'>
-							<Icon boxSize='30px' as={PiGearSixFill} />
-							<Text fontSize='xs'>Settings</Text>
-						</Link>
-					</Button>
-					<Button display='block' variant='link' colorScheme='cyan'>
 						<a href="https://forms.gle/uLbxD1v2XmXhXfer7" target="_blank" rel="noopener noreferrer">
 							<Icon boxSize='30px' as={PiPaperPlaneTiltFill} />
 							<Text fontSize='xs'>FeedBack</Text>
 						</a>
-					</Button>
-					<Button display='block' variant='link' colorScheme='red' onClick={() => signOut({redirect: true, callbackUrl: '/'})}>
-						<Icon boxSize='30px' as={PiSignOutFill} />
-						<Text fontSize='xs'>LogOut</Text>
 					</Button>
 				</Flex>
 			)}
