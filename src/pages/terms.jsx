a70306bb2569f27279042d4bfadc25699c3e6316
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-09-29 23:49:53 +0900

    ✨ モーダルウィンドウで表示できるようになりました。

---
+++ src/pages/terms.jsx
@@ -0,0 +1,43 @@
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+    Button,
+    Box,
+    Text,
+} from '@chakra-ui/react'
+
+
+export default function Terms() {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    return (
+        <>
+            <Text textDecoration="underline" onClick={onOpen}>利用規約</Text>
+    
+            <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+                <ModalHeader>Modal Title</ModalHeader>
+                <ModalCloseButton />
+                <ModalBody>
+                    <Box>
+                        <Text>Lorem ipsum, doloeriam autem sit reprehenderit possimus repudiandae.</Text>
+                    </Box>
+                </ModalBody>
+    
+                <ModalFooter>
+                <Button colorScheme='blue' mr={3} onClick={onClose}>
+                    Close
+                </Button>
+                <Button variant='ghost'>Secondary Action</Button>
+                </ModalFooter>
+            </ModalContent>
+            </Modal>
+        </>
+    )
+}
