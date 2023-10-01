a425a4b44d52efd95ac664ad660894d92b8403f3
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-10-01 16:20:56 +0900

    ✨ #95 Favoriteページの作成

---
+++ src/pages/favorites/index.jsx
@@ -0,0 +1,11 @@
+import { Heading, Text } from '@chakra-ui/react'
+import { DashboardLayout } from '@/components/dashboard-layout'
+
+export default function Favorites() {
+	return (
+		<DashboardLayout>
+			<Heading as='h1' size='lg'>Favorite</Heading>
+			<Text>Coming soon...</Text>
+		</DashboardLayout>
+	)
+}
