939c3593b9eaf8dfada749a1613a3096e3f88bd2
hengin-eer <hirokix0608@gmail.com>
2023-10-11 23:51:47 +0900

    💄 ui: #113 ルートリンクを追加
    ページ内リンクでidがurlについてきてしまうため

--- src/components/page-header.jsx
+++ src/components/page-header.jsx
@@ -1,14 +1,17 @@
 import { Flex, Image } from '@chakra-ui/react'
 import React from 'react'
 import { ButtonLink, GradientText } from './custom-chakra-ui'
+import Link from 'next/link'
 
 export const PageHeader = () => {
     return (
         <Flex align='center' justify='space-between' h='70px' w='100%' >
-            <Flex align='center'>
-                <Image src={'/512x512_Bocchi-Talk.png'} alt='Bocchi Talk' boxSize='50px' />
-                <GradientText fontSize='24px' fontWeight='bold'>Bocchi Talk</GradientText>
-            </Flex>
+            <Link href='/'>
+                <Flex align='center'>
+                    <Image src={'/512x512_Bocchi-Talk.png'} alt='Bocchi Talk' boxSize='50px' />
+                    <GradientText fontSize='24px' fontWeight='bold'>Bocchi Talk</GradientText>
+                </Flex>
+            </Link>
             <ButtonLink href='https://forms.gle/EWAGxPBGrEeKSWJg9' isBlank={true}>お問い合わせ</ButtonLink>
         </Flex>
     )
