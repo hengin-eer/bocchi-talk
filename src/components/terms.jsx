6832f9672dcb1d80da87a32b7e4398b9c7c7088d
QwerTayu <tayu.sa99hire.make@gmail.com>
2023-12-19 05:40:15 +0700

    ✨ #30利用規約とポリシーの英語化対応完了

--- src/components/terms.jsx
+++ src/components/terms.jsx
@@ -10,6 +10,11 @@ import {
     useDisclosure,
     Button,
     Text,
+    Tab,
+    Tabs,
+    TabList,
+    TabPanels,
+    TabPanel
 } from '@chakra-ui/react'
 import ReactMarkdown from 'react-markdown';
 import MarkdownComponents from '@/components/markdownComponents';
@@ -17,6 +22,7 @@ import MarkdownComponents from '@/components/markdownComponents';
 export default function Terms() {
     const { isOpen, onOpen, onClose } = useDisclosure();
     const [ TermsContent, setTermsContent ] = useState('');
+    const [ TermsContentEng, setTermsContentEng ] = useState('');
 
     const fetchPolicyFile = async () => {
         try {
@@ -32,8 +38,23 @@ export default function Terms() {
         }
     };
 
+    const fetchPolicyFileEng = async () => {
+        try {
+            const response = await fetch('/TermsOfServiceEng.md');
+            if (response.ok) {
+                const markdownText = await response.text();
+                setTermsContentEng(markdownText);
+            } else {
+                throw new Error('Failed to fetch policy file. Please reload.');
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
     useEffect(() => {
         fetchPolicyFile();
+        fetchPolicyFileEng();
     }, []);
 
     return (
@@ -43,12 +64,28 @@ export default function Terms() {
             <Modal isOpen={isOpen} onClose={onClose} scrollBehavior="inside">
             <ModalOverlay />
             <ModalContent maxW="90vw">
-                <ModalHeader>利用規約</ModalHeader>
+                <ModalHeader>利用規約 / Terms</ModalHeader>
                 <ModalCloseButton />
                 <ModalBody>
-                    <ReactMarkdown components={MarkdownComponents}>
-                        {TermsContent}
-                    </ReactMarkdown>
+                    <Tabs>
+                        <TabList>
+                            <Tab>日本語</Tab>
+                            <Tab>English</Tab>
+                        </TabList>
+
+                        <TabPanels>
+                            <TabPanel>
+                                <ReactMarkdown components={MarkdownComponents}>
+                                    {TermsContent}
+                                </ReactMarkdown>
+                            </TabPanel>
+                            <TabPanel>
+                                <ReactMarkdown components={MarkdownComponents}>
+                                    {TermsContentEng}
+                                </ReactMarkdown>
+                            </TabPanel>
+                        </TabPanels>
+                        </Tabs>
                 </ModalBody>
     
                 <ModalFooter>
