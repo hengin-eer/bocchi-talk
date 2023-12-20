98351418ebfc015a18cbb5bf42cc12f666248890
hengin-eer <hirokix0608@gmail.com>
2023-12-20 15:54:15 +0900

    feat: #124 プロンプトがぐっと良くなりました！

--- src/states/chatThemeState.js
+++ src/states/chatThemeState.js
@@ -4,6 +4,6 @@ export const systemPromptState = atom({
     key: "systemPrompt",
     default: {
 		role: "system",
-		content: "You are my friend." // 初期値としてシステムメッセージを入れておく。
+		content: "You are my best friend. Always return in one to three brief sentences. You don't always have to answer the question, but please respond with words that will keep the conversation going." // 初期値としてシステムメッセージを入れておく。
 	}
 })
